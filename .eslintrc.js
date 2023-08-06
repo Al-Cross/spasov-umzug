8e5291b4aff3c49d7c6b01d58bd8cdc2634b034f
Al-Cross <krastanov87@gmail.com>
2023-08-06 14:41:01 +0200

    apply eslint rules

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'plugin:vue/vue3-strongly-recommended',
+  ],
+  rules: {
+    "vue/multi-word-component-names": 0,
+    "vue/first-attribute-linebreak": ["error", {
+      "singleline": "ignore",
+      "multiline": "ignore"
+    }],
+    "vue/max-attributes-per-line": ["error", {
+      "singleline": {
+        "max": 5
+      },
+      "multiline": {
+        "max": 4
+      }
+    }],
+    "vue/html-closing-bracket-newline": ["error", {
+      "multiline": "never"
+    }],
+    "vue/singleline-html-element-content-newline": "off",
+    "vue/multiline-html-element-content-newline": "off",
+    "vue/html-self-closing": ["error", {
+      "html": {
+        "normal": "any",
+        "component": "always"
+      }
+    }],
+    "vue/html-indent": ["error", "tab"]
+  }
+}
